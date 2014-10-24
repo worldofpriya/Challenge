369c7da8d1aa5f54901660cc095fd2e96845eb31
pri <priyaramachandaran@gamestop.com>
2014-10-24 11:30:54 -0500

    more stuff

--- ShoeSwap/ShoeSwap/Controllers/HomeController.cs
+++ ShoeSwap/ShoeSwap/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShoeSwap.Models;
 
 namespace ShoeSwap.Controllers
 {
@@ -10,6 +11,19 @@ namespace ShoeSwap.Controllers
     {
         public ActionResult Index()
         {
+            return View("ShoeSwap");
+        }
+
+        [HttpGet]
+        public ActionResult ShoeSwap()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ShoeSwap(ShoeSwapInfo ss)
+        {
+            var info = ss;
             return View();
         }
     }
---
+++ ShoeSwap/ShoeSwap/Models/ShoeSwapInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoeSwap.Models
+{
+    public class ShoeSwapInfo
+    {
+        public string ShoeColor { get; set; }
+        public string ShoeType { get; set; }
+        public string ShoeSize { get; set; }
+        public bool Sock { get; set; }
+        public string ShoeLattitude { get; set; }
+        public string ShoeLongitude { get; set; }
+
+    }
+}
