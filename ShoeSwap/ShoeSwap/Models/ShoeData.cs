2f820b56fa47db1c16a7c7c0a518755afe6a62b4
pri <priyaramachandaran@gamestop.com>
2014-10-24 10:32:34 -0500

    added

---
+++ ShoeSwap/ShoeSwap/Models/ShoeData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoeSwap.Models
+{
+    public class ShoeData
+    {
+        public string ShoeColor { get; set; }
+        public string ShoeType { get; set; }
+        public int ShoeSize { get; set; }
+        public bool Sock { get; set; }
+        public string lattitude { get; set; }
+        public string longitude { get; set; }
+    }
+}
